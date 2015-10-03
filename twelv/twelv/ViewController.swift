6d03cdef8900f365aea365d6178306549d40c5f9
LucasBullen <13lb45@queensu.ca>
2015-10-03 19:57:18 -0400

    iOS App Skeleton
    Base app added

---
+++ twelv/twelv/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twelv
+//
+//  Created by Lucas Bullen on 2015-10-03.
+//  Copyright © 2015 Lucas Bullen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
