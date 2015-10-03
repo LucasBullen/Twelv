393aa4817d69e1aee57a77adf7eaf74eaca980c9
Matthew Pollack <m@matt-3.local>
2015-10-03 16:25:12 -0400

    lol forgot to change title from cisc 282 a2 because I just copied html heh

--- apidoc.php
+++ apidoc.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
 <html xmlns="http://www.w3.org/1999/xhtml" class="no-js">
    <head>
-      <title>CISC 282 A2</title>
+      <title>API Doc</title>
       <meta name="description" content="">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
