27330a90aa2547ef424d31897dc08671ac94e89c
LucasBullen <13lb45@queensu.ca>
2015-10-09 23:58:57 -0400

    objects and constraints added to main menu
    Added all the required elements to the main page that will be required
    for the alpha.

--- iOSTwelv/iOSTwelv/ViewController.swift
+++ iOSTwelv/iOSTwelv/ViewController.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //UIView used to hold images of friends invited to currently viewed event
+    @IBOutlet weak var invitedFriends: UIView!
+    //UITextView used to hold the descriptive text if any of the currently viewed event
+    @IBOutlet weak var descriptionText: UITextView!
+    //UILable used to store locaiton of currently viewed event
+    @IBOutlet weak var location: UILabel!
+    //Digital time shown within clock for event/timer creation
+    @IBOutlet weak var onClockTime: UILabel!
+    //UIImageView is the hour hand of the analog clock
+    @IBOutlet weak var hourHand: UIImageView!
+    //UILable found at top of screen hosting important information and title of the currently viewed event
+    @IBOutlet weak var topInfoBar: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
